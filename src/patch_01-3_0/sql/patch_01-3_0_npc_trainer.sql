87c2e907f27605f88817a27941f6009e0e96ec57
Revision <tkn963@gmail.com>
2023-02-08 14:40:56 +0100

    Flight Form
    I forgot to add this..

--- src/patch_01-3_0/sql/patch_01-3_0_npc_trainer.sql
+++ src/patch_01-3_0/sql/patch_01-3_0_npc_trainer.sql
@@ -2,7 +2,10 @@ UPDATE `npc_trainer` SET `MoneyCost`=350000, `ReqLevel`=30 WHERE `SpellID`=33388
 UPDATE `npc_trainer` SET `MoneyCost`=6000000, `ReqLevel`=60 WHERE `SpellID`=33391; -- Journeyman Riding
 UPDATE `npc_trainer` SET `MoneyCost`=8000000, `ReqLevel`=70 WHERE `SpellID`=34090; -- Expert Riding
 
-DELETE FROM npc_trainer WHERE ID=200006 AND SpellID=33950; -- Flight Form (Shapeshift)
+DELETE FROM npc_trainer WHERE ID=200006 AND SpellID=33950;
+INSERT INTO `npc_trainer` (`ID`, `SpellID`, `MoneyCost`, `ReqLevel`) VALUES
+-- Druid
+(200006, 33950, 81000, 68); -- Flight Form (Shapeshift)
 
 DELETE FROM `npc_trainer` WHERE `ID`=200020 AND `SpellID` IN (
     13820, -- Summon Warhorse
